e346509d726d6a516dc0b8e52ef431520f2ad950
Alan Gutierrez <alan@prettyrobots.com>
2015-09-13 12:39:10 -0500

    Implement `unlisten` to turn a listener off.

---
+++ lib/unlisten.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+homeport module <<-usage
+    usage: homeport <homeport options> unlisten <unlisten options>
+
+    homeport options:
+
+        -t, --tag <string>
+            an optional tag for the image so you can create different images
+usage
+
+trap cleanup EXIT SIGTERM SIGINT
+
+function cleanup() {
+    local pids=$(jobs -pr)
+    [ -n "$pids" ] && kill $pids
+    [ ! -z "$dir" ] && rm -rf "$dir"
+}
+
+dir=$(mktemp -d -t homeport_unlisten.XXXXXXX)
+
+docker kill "$homeport_image_name"
+docker rm "$homeport_image_name"
