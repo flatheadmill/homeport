7bd895ad52aa65eb35fdb11d1487d0c818c2698b
Alan Gutierrez <alan@prettyrobots.com>
2015-09-15 15:20:06 -0500

    Create command to display Homeport path.
    Closes #43.

---
+++ lib/path.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+homeport module <<-usage
+    usage: homeport path
+
+    description:
+
+        Print the location of the Homeport directory. Useful if you you're
+        trying to locate the default Homeport formulas.
+usage
+
+echo $HOMEPORT_PATH
