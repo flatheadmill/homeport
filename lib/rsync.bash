8d32bdec8f0e780a8b653c4eb4a45d430c7e0a6b
Demarius Chrite <chrite.demarius@gmail.com>
2015-10-01 14:44:08 -0400

    Flesh sketch of `homeport rsync`.

--- lib/rsync.bash
+++ lib/rsync.bash
@@ -1,24 +1,36 @@
-rsync -av -e 'ssh -p 32768' ~/Sync/PrettyRobots/DetroitShapeFiles/ alan@192.168.99.100:~/DetroitShapeFiles/
+#!/bin/bash
+#rsync -av -e 'ssh -p 32768' ~/Sync/PrettyRobots/DetroitShapeFiles/ alan@192.168.99.100:~/DetroitShapeFiles/
+ 
+homeport module <<-usage
+    usage: homeport append
 
-"$@"
+    options:
 
-declare -a arguments
+        -t, --tag <string>
+            an optional tag for the image so you can create different images
+usage
 
-arguments=('-e' 'ssh -p '${port})
+arguments=('-av' '-e' 'ssh -p '$(docker port $homeport_image_name 22 | cut -d: -f2))
+
+if [ -z "$DOCKER_HOST" ]; then
+    ip=$(docker inspect --format '{{ .NetworkSettings.Gateway }}' "$homeport_image_name")
+else
+    ip=$(echo "$DOCKER_HOST" | sed 's/^tcp:\/\/\(.*\):.*$/\1/')
+fi
+
 
 while [ $# -ne 0 ]; do
     case "$1" in
         homeport:*)
             value=${1#homeport:}
-            value=${user}@${ip}:${value}
+            value=${homeport_unix_user}@${ip}:${value}
             arguments+=("$value")
             shift
             ;;
         *)
-            arguments+=("$1")
             shift
             ;;
     esac
 done
 
-echo ${arguments}
+echo ${arguments[@]}
