7383b3a62ae98590a082ade6bf8f70589b4a600c
Demarius Chrite <chrite.demarius@gmail.com>
2015-10-01 15:15:59 -0400

    Cleanup usage.

--- lib/rsync.bash
+++ lib/rsync.bash
@@ -1,24 +1,18 @@
 #!/bin/bash
+
 #rsync -av -e 'ssh -p 32768' ~/Sync/PrettyRobots/DetroitShapeFiles/ alan@192.168.99.100:~/DetroitShapeFiles/
  
 homeport module <<-usage
-    usage: homeport append
-
-    options:
-
-        -t, --tag <string>
-            Homeport image tag to rsync to.
+    usage: homeport rsync 
 usage
 
-arguments=('-av' '-e' 'ssh -p '$(docker port $homeport_image_name 22 | cut -d: -f2))
+arguments=('-e' 'ssh -p '$(docker port $homeport_image_name 22 | cut -d: -f2))
 
 if [ -z "$DOCKER_HOST" ]; then
     ip=$(docker inspect --format '{{ .NetworkSettings.Gateway }}' "$homeport_image_name")
 else
     ip=$(echo "$DOCKER_HOST" | sed 's/^tcp:\/\/\(.*\):.*$/\1/')
 fi
-
-
 
 while [ $# -ne 0 ]; do
     case "$1" in
