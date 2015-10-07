268e7387690846a92b9237dde6812d8680909369
Alan Gutierrez <alan@prettyrobots.com>
2015-10-07 07:45:31 -0500

    More sketch of Samba.

---
+++ lib/ip.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+homeport module <<-usage
+    usage: homeport ip
+usage
+
+if [ -z "$DOCKER_HOST" ]; then
+    docker inspect --format '{{ .NetworkSettings.Gateway }}' "$homeport_image_name"
+else
+    echo "$DOCKER_HOST" | sed 's/^tcp:\/\/\(.*\):.*$/\1/'
+fi
---
+++ lib/share.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo $homeport_unix_user
