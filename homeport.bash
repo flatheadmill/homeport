ea139d3afcb5f4b147c73b9e2702b55710d0d653
Alan Gutierrez <alan@prettyrobots.com>
2015-10-14 15:14:07 -0400

    Share home directory through Samba.

--- homeport.bash
+++ homeport.bash
@@ -92,9 +92,18 @@ function homeport_exec() {
 
     shift
 
+    # todo: you're never using this and you're always building, should this be the namespace?
+    if [ ! -z "$homeport_docker_hub_account" ]; then
+        homeport_image_name="${homeport_docker_hub_account}/"
+    else
+        homeport_image_name=
+    fi
+    homeport_image_name+=homeport_${USER}_${homeport_unix_user}_${homeport_namespace}_${homeport_tag}
+    homeport_home_volume="homeport_${USER}_${homeport_unix_user}_home"
+
     export homeport_namespace="$homeport_docker_hub_account"
     export HOMEPORT_PATH homeport_docker_hub_account homeport_unix_user homeport_tag homeport_image_name homeport_unix_user homeport_home_volume
-    export -f usage abend getopt homeport
+    export -f usage abend getopt homeport homeport_exec
 
     "$action" "$@"
 }
@@ -160,12 +169,5 @@ while true; do
             ;;
     esac
 done
-
-if [ ! -z "$homeport_docker_hub_account" ]; then
-    homeport_image_name="${homeport_docker_hub_account}/"
-fi
-homeport_image_name+=homeport_${USER}_${homeport_unix_user}_${homeport_namespace}_${homeport_tag}
-
-homeport_home_volume="homeport_${USER}_${homeport_unix_user}_home"
 
 homeport_exec "$@"
--- lib/share.bash
+++ lib/share.bash
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-echo $homeport_unix_user
+if [ -z "$DOCKER_HOST" ]; then
+    homeport_host=$(docker inspect --format '{{ .NetworkSettings.Gateway }}' "$homeport_image_name")
+else
+    homeport_host=$(echo "$DOCKER_HOST" | sed 's/^tcp:\/\/\(.*\):.*$/\1/')
+fi
+
+export homeport_tag=samba
+export homeport_namespace=bigeasy
+
+if [ $(( $(docker images | awk -v user=$USER -v homeport_unix_user=$homeport_unix_user '$1 == "homeport_" user "_" homeport_unix_user "_bigeasy_samba" { print }' | wc -l) )) -eq 0 ]; then
+    homeport_exec create
+    homeport_exec append samba
+fi
+
+password=$(homeport_exec ssh sudo cat /etc/samba/password 2>/dev/null)
+
+if [ -z "$password" ]; then
+    homeport_exec ssh sudo /usr/share/homeport/smbd $homeport_unix_user
+fi
+
+echo "smb://$homeport_unix_user:$password@$homeport_host"
