11a53f6cd0610c2b405809810894538842e5e485
Alan Gutierrez <alan@prettyrobots.com>
2015-10-27 00:17:05 -0400

    Resurrect `rsync`.

--- lib/rsync.bash
+++ lib/rsync.bash
@@ -7,6 +7,7 @@ homeport module <<-usage
 usage
 
 homeport_emit_evaluated "$@" && exit
+homeport_labels $1 && shift
 
 dir=$(mktemp -d -t homeport_rsync.XXXXXXX)
 
@@ -22,14 +23,14 @@ if [[ "$dir" ==  *' '* ]]; then
     abend "Cannot use a temp directory that contains spaces. TMPDIR=$TMPDIR"
 fi
 
-docker cp "$homeport_image_name":/etc/ssh/ssh_host_rsa_key.pub "$dir/ssh_host_rsa_key.pub"
+docker cp "$homeport_container_name":/etc/ssh/ssh_host_rsa_key.pub "$dir/ssh_host_rsa_key.pub"
 
 if [ -z "$DOCKER_HOST" ]; then
-    ssh_host=$(docker inspect --format '{{ .NetworkSettings.Gateway }}' "$homeport_image_name")
+    ssh_host=$(docker inspect --format '{{ .NetworkSettings.Gateway }}' "$homeport_container_name")
 else
     ssh_host=$(echo "$DOCKER_HOST" | sed 's/^tcp:\/\/\(.*\):.*$/\1/')
 fi
-ssh_port=$(docker port $homeport_image_name 22 | cut -d: -f2)
+ssh_port=$(docker port $homeport_container_name 22 | cut -d: -f2)
 
 echo "[$ssh_host]:$ssh_port $(cut -d' ' -f1,2 < $dir/ssh_host_rsa_key.pub)" > "$dir/known_hosts"
 
@@ -38,7 +39,7 @@ while [ $# -ne 0 ]; do
     case "$1" in
         homeport:*)
             value=${1#homeport:}
-            value=${homeport_unix_user}@${ssh_host}:${value}
+            value=homeport@${ssh_host}:${value}
             arguments+=("$value")
             shift
             ;;
