2ada29b974ac708bbe8fd135734ba9f9b7d07241
Alan Gutierrez <alan@prettyrobots.com>
2015-11-13 14:25:38 -0500

    Expose Docker inside Homeport, specify home dir.
    More than enough Docker inside homeport, including mappings to your home
    directory, but not to other operating system directories. That is, if
    you map a volume in your home directory in Homeport, it will reference
    the same directory in the host machine.
    
    Also in this commit, the ability to specify a home directory to `docker
    run` using the `--home` switch.
    
    Closes #80.
    Closes #84.

--- bin/run.bash
+++ bin/run.bash
@@ -18,16 +18,42 @@ function cleanup() {
 }
 
 ssh_options=''
-docker_options=''
+docker_options='-v /usr/local/bin/docker:/var/lib/homeport/bin/docker:rw '
+docker_options+='-v '$(printf %q "$DOCKER_CERT_PATH:/var/lib/homeport/etc/certs:ro")' '
+docker_options+='-e DOCKER_CERT_PATH=/var/lib/homeport/etc/certs '
+docker_options+='-e DOCKER_TLS_VERIFY=1 '
+docker_options+='-e DOCKER_HOST='$(printf %q "$DOCKER_HOST")' '
 
 declare argv
-argv=$(getopt --options +e:v:p:A --long label:,privileged,docker,volumes-from:,link: -- "$@") || exit 1
+argv=$(getopt --options +e:v:p:A --long home:,label:,privileged,docker,volumes-from:,link: -- "$@") || exit 1
 eval "set -- $argv"
 
 docker_rm=1 named=0 daemonize=0
 
+homeport_home=$HOME
+
 while true; do
     case "$1" in
+        --home)
+            shift
+            homeport_home=$1
+            shift
+            homeport_dir=$homeport_home
+            while [ -L "$homeport_home" ]; do
+                expanded=$(homeport_readlink "$homeport_home")
+                pushd "${homeport_home%/*}" > /dev/null
+                pushd "${expanded%/*}" > /dev/null
+                homeport_dir=$(pwd)
+                popd > /dev/null
+                popd > /dev/null
+                homeport_home="$homeport_dir/${homeport_home##*/}"
+            done
+            homeport_dir=$homeport_home
+            while [ "$HOME" != "$homeport_dir"  ]; do
+                homeport_dir=${homeport_dir%/*}
+                [ -z "$homeport_dir" ] && abend "Home directory must be a child of host home directory."
+            done
+            ;;
         --docker)
             if which docker-machine > /dev/null; then
                 host_docker="/usr/local/bin/docker"
@@ -61,7 +87,7 @@ while true; do
     esac
 done
 
-homeport_home="$HOME:/home/homeport:rw"
+homeport_home_volume="$HOME:/home/homeport:rw"
 
 mkdir -p "$HOME/.homeport"
 rm -f "$HOME/.homeport/touch"
@@ -69,13 +95,14 @@ touch "$HOME/.homeport/touch"
 
 while read -r line; do
     exclude+="${line%%=*}="
-done < <(docker run -v "$homeport_home" --rm $homeport_image bash -c 'printenv')
+done < <(docker run -v "$homeport_home_volume" --rm $homeport_image bash -c 'printenv')
 
 docker='docker run '
+docker+='-e HOMEPORT_HOST_HOME='$homeport_home' '
 docker+='-P -d --privileged '
 docker+='--name '$homeport_container' '
 docker+='--label io.homeport=true '
-docker+='-v '$homeport_home' '
+docker+='-v '$homeport_home_volume' '
 docker+='-h '$homeport_tag' '
 docker+=$docker_options
 docker+=$homeport_image' '
