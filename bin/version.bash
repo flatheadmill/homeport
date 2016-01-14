4e72a475ac278248578a347a18242fa77b373ccf
Alan Gutierrez <alan@prettyrobots.com>
2016-01-14 14:26:30 -0600

    Implement a version display.

---
+++ bin/version.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+homeport module <<-usage
+    usage: homeport version
+
+    description:
+
+        Print the current version of Homeport.
+usage
+
+homeport_emit_evaluated && exit
+
+echo "1.0.6-pre"
