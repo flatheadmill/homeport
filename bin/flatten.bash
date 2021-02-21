b6907f99b610996921dd78234452d1d124966e4a
Alan Gutierrez <alan@prettyrobots.com>
2021-02-21 15:39:18 -0600

    Committing to new single file Homeport.

--- bin/flatten.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-homeport module <<-usage
-    usage: homeport flatten
-usage
-
-"$homeport_path/lib/export" --archive - | "$homeport_path/lib/import" --archive -
--- bin/github.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo https://github.com/bigeasy/homeport
